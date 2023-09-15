3e0de8c3d6f67f39a12cea2f1aa909d4b8694feb
Byung-In KIM <kimbi@cliensoft.com>
2023-09-15 17:36:13 +0900

    Run "ng add @angular/pwa"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 // Angular Modules
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 // App Components
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,12 @@ import { WeatherComponent } from './weather/weather.component';
     MatIconModule,
     MatInputModule,
     MatCardModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]

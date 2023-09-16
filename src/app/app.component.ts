a276eee3e74e7dec86bac47e434d5320ec9daf67
Andrew KIM <andrewbkim.dev@gmail.com>
2023-09-16 15:45:22 +0900

    Add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject, switchMap, takeUntil, tap } from 'rxjs';
+
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(
+    private updates: SwUpdate,
+    private snackBar: MatSnackBar,
+  ){}
+
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.checkForVersionUpdates().subscribe(res => {
+      if (res) {
+        this.showUpdateSnackBar();
+      }
+    });
+  }
+
+  private checkForVersionUpdates(): Observable<boolean> {
+    return this.updates.versionUpdates.pipe(
+      tap((res: VersionEvent) => this.logVersionEvent(res)),
+      switchMap(() => this.updates.activateUpdate().then(() => true)),
+      takeUntil(this.destroy$)
+    );
+  }
+
+  private showUpdateSnackBar(): void {
+    const snackBarRef = this.snackBar.open('A new version is available!', 'Update now');
+    snackBarRef.afterDismissed().subscribe(result => {
+      if (result.dismissedByAction) {
+        location.reload();
+      }
+    });
+  }
+
+  private logVersionEvent(event: VersionEvent): void {
+    // You can use a more advanced logging mechanism here
+    console.log(event);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,37 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 // Angular Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // App Components
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatInputModule,
-    MatCardModule,
+    MatSnackBarModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
